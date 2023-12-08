88f45e944cffe4132b90150359ed6c6d891f2f96
Felipe Conde <felipe.conde@sptech.school>
2023-12-08 04:48:05 -0300

    reformulação do mapa

--- script.js
+++ script.js
@@ -37,39 +37,26 @@ var listaQuestao = [
 ];
 
 var listaPosicao = [
-  ["i248", "i265", "i282"], /* i248 C, i265 P, i282 U */
-  ["i282", "i283", "i284"],/* i282 U i283 L i284 A */
-  [
-    "i73",
-    "i74",
-    "i75",
-    "i76",
-    "i77",
-    "i78",
-    "i79",
-    "i80",
-    "i81",
-    "i82",
-    "i83",
-    "i84",
-    "i85",
-  ],
-  ["i79", "i96", "i113"],
-  ["i214", "i215", "i216"],
-  ["i212", "i213", "i214", "i215", "i216"],
-  ["i115", "i116", "i117", "i118", "i119"],
-  ["i216", "i233", "i250", "i267", "i284"],
-  ["i112", "i113", "i114"],
-  ["i198", "i199"],
-  ["i16", "i33", "i50", "i67", "i84", "i101", "i118", "i135", "i152", "i169"],
-  ["i97", "i114", "i131", "i148", "i165", "i182", "i199"],
+  ["i171", "i172", "i173"], /* i248 C, i265 P, i282 U */
+  ["i173", "i190", "i207"],/* i282 U, i283 L, i284 A */
+  ["i73", "i74", "i75", "i76", "i77", "i78", "i79", "i80",  "i81", "i82", "i83", "i84", "i85"], // i73 R, i74 E, i75 G, i76 I, i77 S, i78 T, i79 R, i80 A, i81 D, i82 O, i83 R, i84 E, i85 S
+  ["i79", "i96", "i113"], // i79 R, i96 A, i113 M
+  ["i211", "i228", "i245"], // i211 R, i228 O, i245 M
+  ["i212", "i213", "i214", "i215", "i216"], // i212 E, i213 P, i214 R, i215 O, i216 M
+  ["i166", "i167", "i168", "i169", "i170"], // i166 F, i167 L, i168 A, i169 S, i170 H
+  ["i216", "i233", "i250", "i267", "i284"], // i216 M, i233 A, i250 S, i267 S, i284 A
+  ["i14", "i15", "i16"], // i14 D, i15 M, i16 A
+  ["i124", "i125"], // i124 C, i125 S
+  ["i16", "i33", "i50", "i67", "i84", "i101", "i118", "i135", "i152", "i169"], 
+  ["i181", "i182", "i183", "i184", "i185", "i186", "i187"],
+  ["i76", "i59"],
   ["i76", "i93"],
-  ["i59", "i60"],
   ["i205", "i206", "i207", "i208", "i209", "i210", "i211", "i212"],
   ["i130", "i147", "i164", "i181", "i198", "i215", "i232", "i249"],
-  ["i157", "i158", "i159", "i160", "i161", "i162", "i163"],
-  ["i144", "i161", "i178", "i195", "i212"],
+  ["i23", "i40", "i57", "i74", "i91", "i108", "i125"],
+  ["i124", "i141", "i158", "i175", "i192"],
 ];
+
 var palavras = [
   /* 1 */ "CPU",
   /* 2  */ "ULA",
@@ -91,75 +78,88 @@ var palavras = [
   /* 18 */ "CACHE",
 ];
 
+
 /* Primeiro For Gera a numeração da cruzada */
-for (var i = 1; i <= 18; i++) {
-  corpo.innerHTML += `<div id="inicio${i}">${i}</div>`;
+for (var NumQuestao = 1; NumQuestao <= 18; NumQuestao+=1) {
+  corpo.innerHTML += `<div id="inicio${NumQuestao}">${NumQuestao}</div>`;
 }
 
 /* For gera a pergunta e a numeração*/
-for (var i = 1; i <= 18; i++) {
-  perguntas.innerHTML += `<div id="pergunta${i}"><p><span id="qNum">${i}.</span>${listaQuestao[i-1]}</p></div>`;
+for (var questao = 1; questao <= 18; questao+=1) {
+  perguntas.innerHTML += `<div id="pergunta${questao}"><p><span id="qNum">${questao}.</span>${listaQuestao[questao - 1]}</p></div>`;
 }
 
 /* For gera todos os quadrados vazios e desativados*/
-for (var i = 1; i <= 289; i++) {
-  linha.innerHTML += `<input onkeyup="veriClica()" style='border:none;background:none;' maxlength="1" type="text" id="i${i}" disabled="true">`;
+for (var posicao = 1; posicao <= 289; posicao+=1) {
+  linha.innerHTML += `<input onkeyup="VerificarResposta()" style='border:none;background:none;' maxlength="1" type="text" id="i${posicao}" disabled="true">`;
 }
 
 /* For que desenha e habilita input */
-for (var i = 16; i < 186; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 171; box < 174; box += 1){ // CPU
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 59; i <= 60; i++) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 173; box < 224; box += 17){ // ULA
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 85; i > 72; i--) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for (var box = 73; box <= 85; box += 1) { // REGISTRADORES
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 76; i < 94; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for (var box = 76; box < 94; box += 17) { // I7
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 119; i > 111; i--) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 79; box < 266; box += 17){ // RAM, QUADCORE
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 79; i < 266; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 228; box < 262; box += 17){ // ROM
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 80; i < 301; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 205; box < 217; box += 1){ // DUALCORE, EPROM
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 165; i > 156; i--) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 233; box <= 284; box += 17){ // MASSA
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 144; i < 229; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 181; box <= 187; box += 1){ // DATABUS
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 205; i < 217; i++) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 166; box <= 170; box += 1){ //FLASH
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 248; i < 299; i += 17) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 59; box < 60; box += 1){ //I5
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
-for (var i = 282; i < 285; i++) {
-  document.getElementById(`i${i}`).style.border = "1px solid black";
-  document.getElementById(`i${i}`).disabled = false;
+for(var box = 14; box <= 16; box += 1){ //DMA
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
+}
+for(var box = 124; box < 209; box += 17){ //CACHE
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
+}
+for(var box = 23; box < 142; box += 17){ //THREADS, CS
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
+}
+for(var box = 33; box < 169; box += 17){ //ADDRESSBUS
+  document.getElementById(`i${box}`).style.border = "1px solid black";
+  document.getElementById(`i${box}`).disabled = false;
 }
 
-function veriClica() {
-  for (var contador = 0; contador < palavras.length; contador++) {
-    var palavraAtual = ""; 
-    for (var contadorPosicao = 0; contadorPosicao < listaPosicao[contador].length; contadorPosicao++) {
+function VerificarResposta() {
+  for (var contador = 0; contador < palavras.length; contador+=1) {
+    var palavraAtual = "";
+    for (var contadorPosicao = 0; contadorPosicao < listaPosicao[contador].length; contadorPosicao+=1) {
 
       palavraAtual += document.getElementById(listaPosicao[contador][contadorPosicao]).value.toUpperCase();
       console.log(palavraAtual);
